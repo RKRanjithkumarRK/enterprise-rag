945a8c21b3163879427801545611fca72278df7d
Ranjithkumar Ramesh <ranjithkumar24072004@gmail.com>
2026-02-21 16:00:03 +0530

    Reduce memory usage for free tier

--- src/retrieval/retrieve_chunks.py
+++ src/retrieval/retrieve_chunks.py
@@ -1,15 +1,10 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-
 def retrieve_chunks(collection, query, top_k=5):
-
+    model = SentenceTransformer("all-MiniLM-L6-v2")
     query_embedding = model.encode(query).tolist()
-
     results = collection.query(
         query_embeddings=[query_embedding],
         n_results=top_k
     )
-
     return results
